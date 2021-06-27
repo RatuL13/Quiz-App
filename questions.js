beb70f6b1adeabb30e6e50d082a5a7cb2011bad7
RatuL13 <54267395+RatuL13@users.noreply.github.com>
2021-06-27 19:53:32 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How do you instantiate an array in Java?",
+    answer: "int arr[] = new int[3];",
+    options: [
+      "int arr[] = new int(3);",
+      "int arr[];",
+      "int arr[] = new int[3];",
+      "int arr() = new int(3);",
+    ]
+  },
+    {
+    numb: 2,
+    question: "When does the ArrayIndexOutOfBoundsException occur?",
+    answer: "Run-time",
+    options: [
+      "Compile-time",
+      "Run-time",
+      "Not an error",
+      "Not an exception at all"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Process of removing an element from stack is called __________",
+    answer: "Pop",
+    options: [
+      "Create",
+      "Push",
+      "Evaluation",
+      "Pop"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not the application of stack?",
+    answer: "Data Transfer between two asynchronous process",
+    options: [
+      "A parentheses balancing program",
+      "Tracking of local variables at run time",
+       "Compiler Syntax Analyzer",
+       "Data Transfer between two asynchronous process"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The data structure required for Breadth First Traversal on a graph is?",
+    answer: "Queue",
+    options: [
+      "Stack",
+      "Array",
+      "Queue",
+      "Tree"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Queues serve major role in ______________",
+    answer: "Simulation of limited resource allocation",
+    options: [
+      "Simulation of recursion",
+      "Simulation of arbitrary linked list",
+      "Simulation of limited resource allocation",
+      "Simulation of heap sort"
+    ]
+  },
+  {
+    numb: 7,
+    question: "A linear collection of data elements where the linear node is given by means of pointer is called?",
+    answer: "Linked list",
+    options: [
+      "Linked list",
+      "Node list",
+      "Primitive list",
+      "Unordered list"
+    ]
+  },
+  {
+    numb: 8,
+    question: " In linked list each node contains a minimum of two fields. One field is data field to store the data second field is?",
+    answer: "Pointer to node",
+    options: [
+      "Pointer to character",
+      "Pointer to integer",
+      "Pointer to node",
+      "Node"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The number of edges from the node to the deepest leaf is called _________ of the tree.",
+    answer: "Height",
+    options: [
+      "Height",
+      "Depth",
+      "Length",
+      "Width"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the average case time complexity for finding the height of the binary tree?",
+    answer: "h = O(log n)",
+    options: [
+      "h = O(loglogn)",
+      "h = O(nlogn)",
+       "h = O(n)",
+      "h = O(log n)"
+    ]
+  },
+];
